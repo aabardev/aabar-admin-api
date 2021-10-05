eac8a8f700a2784abd9cd068b5b49f909d539b2b
aabardev <aabardev@gmail.com>
2021-10-05 22:18:30 +0530

    complete offer and event api

--- db/dbHandlers.js
+++ db/dbHandlers.js
@@ -136,7 +136,7 @@ export const upsertMenuCategory = async (data) => {
 export const upsertMenuItem = async (data) => {
   // const client = await db.pool.connect();
 
-  // (1) save menu category data into db
+  // (1) save menu item data into db
   try {
     let res = null;
     let actionTaken = null;
@@ -180,6 +180,126 @@ export const upsertMenuItem = async (data) => {
 
     // (2) save image data into db
     if (actionTaken !== "menu_item_deactivated") {
+      let img = null;
+      let refId = res.rows[0].id;
+      for (let i=0; i<data.imgs.length; i++) {
+        img = data.imgs[i];
+        img.refId = refId;
+        res = await upsertImage(img);
+      }
+    }
+    
+    return { status: 'success' }
+  } catch (error) {
+    return { status: 'failed', error: error }
+  }
+};
+
+export const upsertOffer = async (data) => {
+  // (1) save offer data into db
+  try {
+    let res = null;
+    let actionTaken = null;
+    if (data.isActive === "false") {
+      //delete
+      res = await db.query(
+        "UPDATE public.offer SET is_active = $1, updated_on = NOW() WHERE id = $2 RETURNING id;",
+        [data.isActive, data.id]
+      );
+      actionTaken = "offer_deactivated";
+    } else if (data.isActive === "true") {
+      if (data.id) {
+        //edit
+        res = await db.query(
+          `UPDATE public.offer
+          SET 
+            title = COALESCE (NULLIF ($1, ''), title),
+            subtitle = COALESCE (NULLIF ($2, ''), subtitle),
+            description = COALESCE (NULLIF ($3, ''), description),
+            tag = COALESCE (NULLIF ($4, ''), tag),
+            start_date = COALESCE (NULLIF ($5, ''), start_date),
+            end_date = COALESCE (NULLIF ($6, ''), end_date),
+            is_top_offer = COALESCE (NULLIF ($7, ''), is_top_offer::varchar)::boolean,
+            updated_on = NOW()
+          WHERE 
+            id = $8 
+          RETURNING id;`,
+          [data.title, data.subtitle, data.description, data.tag, data.startDate, data.endDate, data.isTopOffer, data.id]
+        );
+        actionTaken = "offer_updated";
+      } else {
+        //insert
+        res = await db.query(
+          "INSERT INTO public.offer (title, subtitle, description, tag, start_date, end_date, is_top_offer, is_active) VALUES ($1, $2, $3, $4, $5, $6, $7, true) RETURNING id;",
+          [data.title, data.subtitle, data.description, data.tag, data.startDate, data.endDate, data.isTopOffer]
+        );
+        actionTaken = "offer_inserted";
+      }
+    }
+    console.log("res");
+    console.log(res);
+
+    // (2) save image data into db
+    if (actionTaken !== "offer_deactivated") {
+      let img = null;
+      let refId = res.rows[0].id;
+      for (let i=0; i<data.imgs.length; i++) {
+        img = data.imgs[i];
+        img.refId = refId;
+        res = await upsertImage(img);
+      }
+    }
+    
+    return { status: 'success' }
+  } catch (error) {
+    return { status: 'failed', error: error }
+  }
+};
+
+export const upsertEvent = async (data) => {
+  // (1) save menu category data into db
+  try {
+    let res = null;
+    let actionTaken = null;
+    if (data.isActive === "false") {
+      //delete
+      res = await db.query(
+        "UPDATE public.event SET is_active = $1, updated_on = NOW() WHERE id = $2 RETURNING id;",
+        [data.isActive, data.id]
+      );
+      actionTaken = "event_deactivated";
+    } else if (data.isActive === "true") {
+      if (data.id) {
+        //edit
+        res = await db.query(
+          `UPDATE public.event
+          SET 
+            title = COALESCE (NULLIF ($1, ''), title),
+            subtitle = COALESCE (NULLIF ($2, ''), subtitle),
+            description = COALESCE (NULLIF ($3, ''), description),
+            start_date = COALESCE (NULLIF ($4, ''), start_date),
+            end_date = COALESCE (NULLIF ($5, ''), end_date),
+            updated_on = NOW()
+          WHERE 
+            id = $6 
+          RETURNING id;`,
+          [data.title, data.subtitle, data.description, data.startDate, data.endDate, data.id]
+        );
+        actionTaken = "event_updated";
+      } else {
+        //insert
+        res = await db.query(
+          "INSERT INTO public.event (title, subtitle, description, start_date, end_date, is_active) VALUES ($1, $2, $3, $4, $5, true) RETURNING id;",
+          [data.title, data.subtitle, data.description, data.startDate, data.endDate]
+        );
+        actionTaken = "event_inserted";
+      }
+    }
+    console.log("res");
+    console.log(res);
+
+    // (2) save image data into db
+    if (actionTaken !== "event_deactivated") {
       let img = null;
       let refId = res.rows[0].id;
       for (let i=0; i<data.imgs.length; i++) {
--- handlers.js
+++ handlers.js
@@ -100,3 +100,103 @@ export const saveMenuItem = async (data) => {
     };
   }
 };
+
+export const saveOffer = async (data) => {
+  console.log(data);
+  let eData = { imgs: [] };
+  let keySplit = null;
+  let res = null;
+
+  // (1) Validate and Prepare the data for upload and save
+  if (data.txt_isActive === 'false') {
+    eData.id = data.txt_id;
+    eData.isActive = data.txt_isActive;
+  } else {
+    for (let key in data) {
+      keySplit = key.split("_");
+      if (keySplit[0] === "txt") {
+        eData[keySplit[1]] = data[key];
+      } else if (keySplit[0] === "img") {
+        // { img_id_typeCode_isPrimary_isActive_filename: content }
+        //eg: { img_4_mc_true_true_breakfast1: [Buffer type] }
+        eData.imgs.push({
+          id: keySplit[1] ? keySplit[1] : null,
+          refId: null,
+          type: imgTypeCodes[keySplit[2]],
+          url: null,
+          dbxPath: null,
+          isPrimary: keySplit[3],
+          isActive: keySplit[4],
+          filename: keySplit[5],
+          content: data[key],
+        });
+      }
+    }
+  }
+  
+  console.log('eData');
+  console.log(eData);
+
+  // (2) Save data to db
+  res = await db.upsertOffer(eData);
+  console.log("RES:", res);
+  if (res.status === "success" ) {
+    return { status: res.status, msg: "Offer saved successfully" };
+  } else {
+    return {
+      status: res.status,
+      error: res.error,
+      msg: "Could not save Offer",
+    };
+  }
+};
+
+export const saveEvent = async (data) => {
+  console.log(data);
+  let eData = { imgs: [] };
+  let keySplit = null;
+  let res = null;
+
+  // (1) Validate and Prepare the data for upload and save
+  if (data.txt_isActive === 'false') {
+    eData.id = data.txt_id;
+    eData.isActive = data.txt_isActive;
+  } else {
+    for (let key in data) {
+      keySplit = key.split("_");
+      if (keySplit[0] === "txt") {
+        eData[keySplit[1]] = data[key];
+      } else if (keySplit[0] === "img") {
+        // { img_id_typeCode_isPrimary_isActive_filename: content }
+        //eg: { img_4_mc_true_true_breakfast1: [Buffer type] }
+        eData.imgs.push({
+          id: keySplit[1] ? keySplit[1] : null,
+          refId: null,
+          type: imgTypeCodes[keySplit[2]],
+          url: null,
+          dbxPath: null,
+          isPrimary: keySplit[3],
+          isActive: keySplit[4],
+          filename: keySplit[5],
+          content: data[key],
+        });
+      }
+    }
+  }
+  
+  console.log('eData');
+  console.log(eData);
+
+  // (2) Save data to db
+  res = await db.upsertEvent(eData);
+  console.log("RES:", res);
+  if (res.status === "success" ) {
+    return { status: res.status, msg: "Event saved successfully" };
+  } else {
+    return {
+      status: res.status,
+      error: res.error,
+      msg: "Could not save Event",
+    };
+  }
+};
